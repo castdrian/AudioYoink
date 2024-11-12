f134cfc9af2a31a4babcc4b5f616bef18b7d8539
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-11-12 12:19:50 +0100

    Initial Commit

---
+++ audioyoink/audioyoinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioyoinkApp.swift
+//  audioyoink
+//
+//  Created by Adrian Castro on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct audioyoinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
